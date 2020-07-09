eb8941506f96bb0e1860aebeb169dfd535c402c2
Harold Mei <haroldmei.cn@gmail.com>
2020-07-09 21:48:12 +1000

    fix the build

--- src/app/containers/HomePage/index.tsx
+++ src/app/containers/HomePage/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
 import { NavBar } from '../NavBar';
-import { Masthead } from './Masthead';
+//import { Masthead } from './Masthead';
 import { PageWrapper } from 'app/components/PageWrapper';
 import { DynamicForm } from 'app/containers/DynamicForm';
 
