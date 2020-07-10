4bba56f5018365ad68c7684556b5990224130a32
Harold Mei <haroldmei.cn@gmail.com>
2020-07-10 21:30:22 +1000

    update backend api

--- src/app/containers/DynamicForm/saga.ts
+++ src/app/containers/DynamicForm/saga.ts
@@ -7,8 +7,8 @@ import { selectKnots } from './selectors';
 
 export function* getForms() {
   yield delay(500);
-  //const engineURL = `https://hmei.me/backend/engine`;
-  const engineURL = `http://localhost:5000/engine`;
+  const engineURL = `https://hmei.me/backend/engine`;
+  //const engineURL = `http://localhost:5000/engine`;
 
   const states = yield select(selectKnots);
 
